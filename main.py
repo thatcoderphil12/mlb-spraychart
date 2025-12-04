85c74bdd307fa181a8643d4e5f32a3bb770f5f22
thatcoderphil12 <gg2nq9djjb@privaterelay.appleid.com>
2025-12-04 23:55:50 +0000

    Added batting_stats import and player ID lookup functionality.

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ import pybaseball
 from pybaseball import statcast
 from pybaseball import  playerid_lookup
 from pybaseball import  statcast_batter
+from pybaseball import batting_stats
+player = input("Please enter the player's name (First Last): ")
+first_name, last_name = player.split()
+id = pybaseball.playerid_lookup(last_name, first_name)['key_mlbam'][0]
+fullstats = statcast_batter(start_dt="2025-03-27", end_dt="2025-09-28", player_id=id)
