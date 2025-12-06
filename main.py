b13504135e30c683f03c68b7fe85dce5d4b30758
thatcoderphil12 <gg2n19djjb@privaterelay.appleid.com>
2025-12-05 20:34:41 -0500

    Added extra imports for graphing and removed unused pybaseball imports, updated extracted_data to remove empty lines, and added empty CSV for refernce

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import pybaseball
-from pybaseball import statcast
-from pybaseball import  playerid_lookup
 from pybaseball import  statcast_batter
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import numpy as np
 player = input("Please enter the player's name (First Last): ")
 first_name, last_name = player.split()
 id = pybaseball.playerid_lookup(last_name, first_name)['key_mlbam'][0]
 fullstats = statcast_batter(start_dt="2025-03-27", end_dt="2025-09-28", player_id=id)
 columns_to_extract = ['hc_x', 'hc_y']
 extracted_data = fullstats[columns_to_extract]
+extracted_data = extracted_data.dropna(subset=['hc_x', 'hc_y'])
 print(extracted_data)
 extracted_data.to_csv('player_stats.csv', index=False)
 print("\nData saved to player_stats.csv")
