6a103bbb9969f33d11fb73f7c1eb5592840539a3
thatcoderphil12 <gg2n19djjb@privaterelay.appleid.com>
2025-12-05 21:19:26 -0500

    Added basic graphing, updated imports, removed CSV debugger

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import pybaseball
-from pybaseball import  statcast_batter
+from pybaseball import statcast_batter
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
-import numpy as np
 player = input("Please enter the player's name (First Last): ")
 first_name, last_name = player.split()
 id = pybaseball.playerid_lookup(last_name, first_name)['key_mlbam'][0]
 fullstats = statcast_batter(start_dt="2025-03-27", end_dt="2025-09-28", player_id=id)
-columns_to_extract = ['hc_x', 'hc_y']
-extracted_data = fullstats[columns_to_extract]
-extracted_data = extracted_data.dropna(subset=['hc_x', 'hc_y'])
-print(extracted_data)
-extracted_data.to_csv('player_stats.csv', index=False)
-print("\nData saved to player_stats.csv")
+coordinates = fullstats[['hc_x', 'hc_y']]
+coordinates = coordinates.dropna(subset=['hc_x', 'hc_y'])
+fig, ax = plt.subplots(figsize=(10, 10))
+ax.add_patch(patches.Wedge((125, 0), 250, 45, 135, ec="black", facecolor="lightgreen", linewidth=2))
+ax.scatter(coordinates['hc_x'], coordinates['hc_y'], alpha=0.5, s=50, color='red')
+ax.set_xlim(-50, 300)
+ax.set_ylim(-50, 300)
+ax.set_aspect('equal')
+ax.set_xlabel('X (feet)')
+ax.set_ylabel('Y (feet)')
+ax.set_title(f'{player} - Spray Chart ({len(coordinates)} hits)')
+ax.grid(True, alpha=0.3)
+plt.tight_layout()
+plt.savefig('spray_chart.png', dpi=300, bbox_inches='tight')
+print(f"Spray chart saved as 'spray_chart.png'")
+plt.show()
